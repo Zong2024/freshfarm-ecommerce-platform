c53e0f3d0bdd97dee82c49972b4feb859cefd994
zong <Zong20021107@gmail.com>
2026-03-06 17:06:03 +0800

    feat(swiper): 引入 Swiper 函式庫並配置 Unsplash 圖片來源

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "", // 如果網址沒有特定 port，留空即可
+        pathname: "/**", // 允許該網域下的所有路徑
+      },
+    ],
+  },
 };
 
 export default nextConfig;
