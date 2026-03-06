231d3448f7fdd6cb9da574e19e69db74ce64ff97
zong <Zong20021107@gmail.com>
2026-03-06 17:04:24 +0800

    feat(frontend): 新增主要佈局

---
+++ app/(frontend)/layout.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "@/components/layout/Footer";
+import { Navbar } from "@/components/layout/Navbar";
+
+export default function FrontendLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen min-w-[320px] flex-col">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
