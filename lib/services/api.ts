19fad6381dde9e6ced987cc62b226f7e6600438f
zong <Zong20021107@gmail.com>
2026-04-05 16:02:36 +0800

    fix(api-url): 修正基礎 API 路徑中缺少的斜線

--- lib/services/api.ts
+++ lib/services/api.ts
@@ -1,1 +1,1 @@
-export const baseUrl = `${process.env.NEXT_PUBLIC_API_URL}/api${process.env.NEXT_PUBLIC_API_PATH}`;
+export const baseUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_PATH}`;
